a0b614cb47d7cc9007d4500feb0ba1986cd66614
TomaszGacek <gacektomasz02@gmail.com>
2024-02-15 13:50:52 +0100

    AddingFirstNotWorkingBack

---
+++ back/SawCarBack/src/main/java/com/sawcar/sawcarback/SawCarBackApplication.java
@@ -0,0 +1,13 @@
+package com.sawcar.sawcarback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SawCarBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SawCarBackApplication.class, args);
+	}
+
+}
---
+++ back/SawCarBack/src/test/java/com/sawcar/sawcarback/SawCarBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sawcar.sawcarback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SawCarBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
