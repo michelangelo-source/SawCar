a92b6ef145d179a00cba7f6ff355ab42cee69fe7
TomaszGacek <gacektomasz02@gmail.com>
2024-02-18 19:43:09 +0100

    SimpleFetch

--- front/scripts/loginScript.js
+++ front/scripts/loginScript.js
@@ -1,5 +1,8 @@
+
 function loginFun(){
     console.log(document.getElementById("inputEmail").value)
     console.log(document.getElementById("inputPassword").value)
-    
+    fetch("http://localhost:8080/users")
+.then(res => res.json())
+.then(data => console.log(data))
 }
