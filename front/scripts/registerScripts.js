fd4c790f93add27bf96a016603e00e79a0c9dfc7
TomaszGacek <gacektomasz02@gmail.com>
2024-02-20 16:49:20 +0100

    registerKlinet

--- front/scripts/registerScripts.js
+++ front/scripts/registerScripts.js
@@ -1,7 +1,7 @@
-// Example POST method implementation:
-async function postData(url = "http://localhost:8080/users", data) {
-    // Default options are marked with *
-    const response = await fetch(url, {
+
+  
+ async function postData(){
+     const res =await fetch("http://localhost:8080/users",{
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, *cors, same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
@@ -12,11 +12,22 @@ async function postData(url = "http://localhost:8080/users", data) {
       },
       redirect: "follow", // manual, *follow, error
       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
-  }
-  
-  function parseData(){
-    console.log(document.registerForm.body())
-  }
+      body: JSON.stringify(RegisterData), // body data type must match "Content-Type" header
+    })
+    return res.json();
+    }
+
+
+
+    function registerUser(){
+      RegisterData={
+        "name":document.getElementById("Name").value,
+        "surname":document.getElementById("Surname").value,
+        "nickname":document.getElementById("Nickname").value,
+        "email":document.getElementById("Email").value,
+        "password":document.getElementById("Password").value
+      }
+      postData().then((data)=>{
+      console.log(data)
+      });
+    }
