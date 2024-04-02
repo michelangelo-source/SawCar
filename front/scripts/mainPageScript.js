34644e8a7e43d44a7cdd3278d9ae94e640bf29d8
TomaszGacek <gacektomasz02@gmail.com>
2024-04-02 23:51:58 +0200

    working logout

--- front/scripts/mainPageScript.js
+++ front/scripts/mainPageScript.js
@@ -25,3 +25,29 @@ async function getUser(url){
 
   });
 }
+
+
+
+
+async function destroyToken(){
+  const res =await fetch("http://localhost:8080/token/"+sessionStorage.getItem("nickname"),{
+   method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+   mode: "cors", // no-cors, *cors, same-origin
+   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+   credentials: "same-origin", // include, *same-origin, omit
+   headers: {
+     "Authorization":sessionStorage.getItem("token") //////////////
+     // 'Content-Type': 'application/x-www-form-urlencoded',
+   },
+   redirect: "follow", // manual, *follow, error
+   referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+   body: JSON.stringify(sessionStorage.getItem("nickname"))
+  })
+ }
+function logout(){
+  
+  destroyToken().then(()=>{
+    window.location.replace("http://127.0.0.1:5500/front/pages/index.html");
+  })
+  
+}
