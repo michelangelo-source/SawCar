5ea82c2ab7ba26863315791fe61677890ee0567b
TomaszGacek <gacektomasz02@gmail.com>
2024-04-23 19:35:38 +0200

    rewoke token

--- front/scripts/mainPageScript.js
+++ front/scripts/mainPageScript.js
@@ -41,13 +41,41 @@ async function destroyToken(){
    },
    redirect: "follow", // manual, *follow, error
    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-   body: JSON.stringify(sessionStorage.getItem("nickname"))
+   
   })
  }
 function logout(){
   
   destroyToken().then(()=>{
     window.location.replace("http://127.0.0.1:5500/front/pages/index.html");
+  })
+  
+}
+
+
+
+
+
+async function renewSession(){
+  const res =await fetch("http://localhost:8080/token",{
+   method: "POST", // *GET, POST, PUT, DELETE, etc.
+   mode: "cors", // no-cors, *cors, same-origin
+   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+   credentials: "same-origin", // include, *same-origin, omit
+   headers: {
+     "Authorization":sessionStorage.getItem("token") //////////////
+     // 'Content-Type': 'application/x-www-form-urlencoded',
+   },
+   redirect: "follow", // manual, *follow, error
+   referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+   body: sessionStorage.getItem("nickname")
+  })
+  return res.json();
+ }
+function renewToken(){
+  renewSession().then((data)=>{
+    tokenFull="Bearer "+data.token
+      sessionStorage.setItem("token",tokenFull);
   })
   
 }
