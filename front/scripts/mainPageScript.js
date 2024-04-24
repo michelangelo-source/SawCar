6d58f39b6f1334a2eec07c6bc47152a9e37a1e66
tgacek <gacektomasz02@gmail.com>
2024-04-24 16:25:56 +0200

    creating div to follow someone

--- front/scripts/mainPageScript.js
+++ front/scripts/mainPageScript.js
@@ -1,3 +1,85 @@
+function createFollowDiv(data){
+  followDiv=document.createElement("div")
+  followDiv.id="followDiv"
+
+
+  nicknameDiv=document.createElement("div")
+  nicknameDiv.innerText=data.nickname
+  nicknameDiv.id="nicknameDiv"
+
+
+
+followBtn=document.createElement("button")
+followBtn.classList.add("stdBtn")
+
+if(data.alreadyFollow){
+  followBtn.innerText="Przestan obserwowac"
+  followBtn.addEventListener("click",unfollow)
+
+}else{
+  followBtn.innerText="obserwuj"
+  followBtn.addEventListener("click",follow)
+}
+
+
+
+followDiv.appendChild(nicknameDiv)
+followDiv.appendChild(followBtn)
+document.getElementById("content").innerHTML=""
+document.getElementById("content").appendChild(followDiv)
+}
+
+async function followRequest(followData){
+  const res =await fetch("http://localhost:8080/follow",{
+   method: "POST", // *GET, POST, PUT, DELETE, etc.
+   mode: "cors", // no-cors, *cors, same-origin
+   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+   credentials: "same-origin", // include, *same-origin, omit
+   headers: {
+     "Authorization":sessionStorage.getItem("token"), //////////////
+     "Content-Type": "application/json",
+   },
+   redirect: "follow", // manual, *follow, error
+   referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+   body: JSON.stringify(followData)
+  })
+ }
+
+
+ async function unfollowRequest(followData){
+  const res =await fetch("http://localhost:8080/follow/"+sessionStorage.getItem("nickname")+"/"+document.getElementById("nicknameDiv").innerText,{
+   method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+   mode: "cors", // no-cors, *cors, same-origin
+   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+   credentials: "same-origin", // include, *same-origin, omit
+   headers: {
+     "Authorization":sessionStorage.getItem("token"), //////////////
+
+   },
+   redirect: "follow", // manual, *follow, error
+   referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  })
+ }
+
+function follow(){
+  followData= {
+    "user":sessionStorage.getItem("nickname"),
+    "followedUser":document.getElementById("nicknameDiv").innerText
+  }
+  followRequest(followData).then(()=>{
+    data={alreadyFollow:true,nickname:document.getElementById("nicknameDiv").innerText}
+    createFollowDiv(data)
+  })
+  
+}
+
+function unfollow(){
+  unfollowRequest().then(()=>{
+    data={alreadyFollow:false,nickname:document.getElementById("nicknameDiv").innerText}
+    createFollowDiv(data)
+  })
+}
+
 async function getUser(url){
   const res =await fetch(url,{
    method: "GET", // *GET, POST, PUT, DELETE, etc.
@@ -15,11 +97,12 @@ async function getUser(url){
  return res.json();
  }
 
-
-    function find(){
+  function find(){
       url="http://localhost:8080/users/"+document.getElementById("Nickname").value+"/"+sessionStorage.getItem("id")
-  getUser(url).then((data)=>{
-    console.log(data)
+    getUser(url).then((data)=>{
+    
+      createFollowDiv(data)
+    
   }).catch(()=>{
     alert("Nie ma takiego uzytkownika")
 
